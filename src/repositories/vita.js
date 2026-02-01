cc00365bc842191242022ed636af29c6b70946e6
JacquesRaoult <raoult.jacques@gmail.com>
2026-02-01 19:59:23 +0100

    Fix repository typo

--- src/repositories/vita.js
+++ src/repositories/vita.js
@@ -3,7 +3,7 @@ import { db } from "../utils/database.js"
 export const synaxarVita = {
     async getVita(id) {
         const rows = await db`
-                SELECT vie_b, vita_long, vita_synaxar
+                SELECT vie_b, vita_long, vita_liturgy
                 FROM vies
                 WHERE id = ${id}
             `
