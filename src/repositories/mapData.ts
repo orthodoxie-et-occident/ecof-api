fb5bc0a1e3cfeff03eb2a5a3928db12572b557a7
JacquesRaoult <raoult.jacques@gmail.com>
2026-04-17 17:51:27 +0200

    Completed map data api

--- src/repositories/mapData.ts
+++ src/repositories/mapData.ts
@@ -2,11 +2,17 @@ import { db } from "../utils/database"
 
 export interface Parish {
     name: string
+    adress: string
+    adress2: string
+    postcode: number
+    city: string
     latitude: number
     longitude: number
+    website: string
+    diocese: string
 }
 
 export async function findAllPoi(): Promise<Parish[]> {
-    const rows = await db`SELECT name, latitude, longitude FROM parishes`
+    const rows = await db`SELECT name, adress, adress2, postcode, city, latitude, longitude, website, diocese FROM parishes`
     return rows as Parish[]
 }
