0c72cb93bcda01f5b2b882e24225248035b79956
JacquesRaoult <raoult.jacques@gmail.com>
2026-05-17 08:21:50 +0200

    Parsing news text as markdown

--- src/services/news.js
+++ src/services/news.js
@@ -1,4 +1,5 @@
 import { allNews } from "../repositories/news"
+import { parseMarkdown } from "../utils/markdown.js"
 
 export async function getNews() {
     const news = await allNews.getNews()
@@ -7,5 +8,10 @@ export async function getNews() {
 
 export async function getNewsById(id) {
     const result = await allNews.getNewsById(id)
-    return result
+    if (!result) return null
+
+    return {
+        ...result,
+        text: parseMarkdown(result.text),
+    }
 }
