03bcb5aa79a8c7b68f185676f65826570f7007f6
JacquesRaoult <raoult.jacques@gmail.com>
2026-06-26 11:14:41 +0200

    Fix2 images API

--- src/modules/images/routes.js
+++ src/modules/images/routes.js
@@ -3,12 +3,10 @@ import { serveStatic } from "hono/bun"
 
 const images = new Hono()
 
-// Serve static files from src/assets
 images.use(
     "/*",
     serveStatic({
-        root: "./src",
-        rewriteRequestPath: (path) => path.replace(/^\/api\/images/, "../../assets"),
+        root: "./src/assets",
     }),
 )
 
