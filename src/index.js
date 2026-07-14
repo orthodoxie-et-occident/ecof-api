5c91bcf44dc37ddff947698c1fbd4b4eca66647a
JacquesRaoult <raoult.jacques@gmail.com>
2026-07-15 01:09:06 +0200

    Fix issue#2 API endpoints config

--- src/index.js
+++ src/index.js
@@ -10,25 +10,12 @@ import synaxar from "./modules/synaxar/routes"
 import images from "./modules/images/routes"
 import privacy from "./modules/privacy/routes"
 import support from "./modules/support/routes"
-import appConfigRouter from "./modules/appConfig/routes"
 import mapData from "./modules/mapData/routes"
 
 const app = new Hono()
 
 app.use("*", cors())
 app.use("*", compress())
-
-app.route("/api/calendar", calendar)
-app.route("/api/parish", parish)
-app.route("/api/vita", vita)
-app.route("/api/news", news)
-app.route("/api/reading", reading)
-app.route("/api/synaxar", synaxar)
-app.route("/api/images", images)
-app.route("/api/privacy", privacy)
-app.route("/api/support", support)
-app.route("/api/app-config", appConfigRouter)
-app.route("/api/map-data", mapData)
 
 app.route("/calendar", calendar)
 app.route("/parish", parish)
@@ -39,7 +26,6 @@ app.route("/synaxar", synaxar)
 app.route("/images", images)
 app.route("/privacy", privacy)
 app.route("/support", support)
-app.route("/app-config", appConfigRouter)
 app.route("/map-data", mapData)
 
 export default {
