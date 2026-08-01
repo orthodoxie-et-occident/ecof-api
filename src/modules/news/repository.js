0f917f046a0ac204a97c4127af33f2f4eabe6395
JacquesRaoult <raoult.jacques@gmail.com>
2026-08-01 10:40:06 +0200

    Remove news slug

--- src/modules/news/repository.js
+++ src/modules/news/repository.js
@@ -3,7 +3,7 @@ import { db } from "../../utils/pg_database"
 export const allNews = {
     async getNews() {
         const rows = await db`
-      SELECT id, title, author, slug, slug_id, published_at
+      SELECT id, title, author, slug_id, published_at
       FROM news
       ORDER BY published_at DESC
     `
@@ -12,7 +12,7 @@ export const allNews = {
 
     async getNewsById(id) {
         const rows = await db`
-        SELECT title, author, slug, published_at, content
+        SELECT title, author, published_at, content
         FROM news
         WHERE id = ${id}
         LIMIT 1
